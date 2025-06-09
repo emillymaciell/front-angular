809c9821e797968948659f3a1f102d52d76a89f4
emillymaciell <emilly.m.limma@gmail.com>
2025-06-09 11:36:11 -0300

    aula1

--- escola-app/src/app/app.component.ts
+++ escola-app/src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { ListaDeDisciplinasComponent } from './lista-de-disciplinas/lista-de-disciplinas.component';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ListaDeDisciplinasComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ escola-app/src/app/lista-de-disciplinas/lista-de-disciplinas.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lista-de-disciplinas',
+  imports: [],
+  templateUrl: './lista-de-disciplinas.component.html',
+  styleUrl: './lista-de-disciplinas.component.css'
+})
+export class ListaDeDisciplinasComponent {
+
+}
